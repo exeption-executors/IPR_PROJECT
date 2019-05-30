2e6cb9053479d42e9e493e4071eee86622574900
Mark Bryzgalov <marik33409@mail.ru>
2019-05-30 18:07:58 +0300

    ClientServiceTest updateClient added

--- src/test/java/services/ClientServiceTest.java
+++ src/test/java/services/ClientServiceTest.java
@@ -1,41 +1,35 @@
 package services;
 
-
 import application.Application;
 import entity.Client;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
-import repository.ClientRepository;
 import service.ClientService;
 
 import java.util.List;
 
 import static org.junit.Assert.*;
 
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 @ComponentScan(basePackages = {"main.java.service"})
 @EnableJpaRepositories(basePackages = {"main.java.repository"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @EntityScan
 public class ClientServiceTest {
-
-
     @Autowired
     ClientService clientService;
 
-
     @Test
     public void insertClientIntoDB() {
-        clientService.insertClient("Mark", "Bryzgalov","marik33409@mail.ru", false);
+        clientService.insertClient("Mark", "Bryzgalov", "marik33409@mail.ru", false);
 
         Client clientFromDB = clientService.findByEmail("marik33409@mail.ru");
 
@@ -46,14 +40,14 @@ public class ClientServiceTest {
     }
 
     @Test
-    public  void getAllClientsFromDB(){
+    public void getAllClientsFromDB() {
         clientService.insertClient("Mark", "Bryzgalov", "marik33409@mail.ru", false);
         clientService.insertClient("Andrey", "Levkin", "andrey@gmail.com", false);
         clientService.insertClient("Evgeny", "Davidov", "evgen@yandex.ru", false);
 
         List<Client> clients = clientService.getAll();
         System.out.println("Количество добавленных клиетов: " + clients.size());
-        assertEquals(clients.size(),3);
+        assertEquals(clients.size(), 3);
     }
 
     @Test
@@ -74,7 +68,7 @@ public class ClientServiceTest {
     }
 
     @Test
-    public  void findClintByEmail() {
+    public void findClintByEmail() {
         clientService.insertClient("Andrey", "Levkin", "andrey@gmail.com", false);
         clientService.insertClient("Mark", "Bryzgalov", "marik33409@mail.ru", false);
         clientService.insertClient("Evgeny", "Davidov", "evgen@yandex.ru", false);
@@ -84,5 +78,28 @@ public class ClientServiceTest {
         System.out.println("Найден клиент с email: " + addedClient.getEmail());
 
         assertEquals("marik33409@mail.ru", addedClient.getEmail());
+    }
+
+    @Test
+    public void updateClient() {
+        clientService.insertClient("Andrey", "Levkin", "andrey@gmail.com", false);
+
+        Client clientFromDB = clientService.findByEmail("andrey@gmail.com");
+
+        System.out.println("Данные добавленного клиента: " + " " +
+                clientFromDB.getName() + " " + clientFromDB.getSurname() + " " +
+                clientFromDB.getEmail());
+
+        clientService.updateClient("Andrew", "Lion", "lionking@mail.ru", true, 1l);
+
+        Client updatedClient = clientService.findByEmail("lionking@mail.ru");
+
+        System.out.println("Обновленные данные клиента: " + " " +
+                updatedClient.getName() + " " + updatedClient.getSurname() + " " +
+                updatedClient.getEmail());
+
+        assertEquals("Andrew", updatedClient.getName());
+        assertEquals("Lion", updatedClient.getSurname());
+        assertEquals("lionking@mail.ru", updatedClient.getEmail());
     }
 }
