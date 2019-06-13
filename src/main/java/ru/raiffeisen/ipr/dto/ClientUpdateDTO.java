9e7aa92d935902ce07c8691162ac57dd11120095
LightWind <OLIGHTWINDO@gmail.com>
2019-06-13 19:49:42 +0300

    REST CLIENT-PLAN-SECTION-POINT CRUD + REFACTORING!!!

---
+++ src/main/java/ru/raiffeisen/ipr/dto/ClientUpdateDTO.java
@@ -0,0 +1,64 @@
+package ru.raiffeisen.ipr.dto;
+
+import ru.raiffeisen.ipr.entity.Client;
+
+public class ClientUpdateDTO {
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private boolean fired;
+    private String password;
+
+    public ClientUpdateDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isFired() {
+        return fired;
+    }
+
+    public void setFired(boolean fired) {
+        this.fired = fired;
+    }
+
+}
