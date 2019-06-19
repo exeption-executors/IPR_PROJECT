6aeb3812dea5d04367ac7097f5916e04550f32bd
jerryngton <jerryngtond@gmail.com>
2019-06-19 10:19:22 +0300

    client controller save client chaanged with return value

--- src/main/java/ru/raiffeisen/ipr/rest/ClientController.java
+++ src/main/java/ru/raiffeisen/ipr/rest/ClientController.java
@@ -2,17 +2,14 @@ package ru.raiffeisen.ipr.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import ru.raiffeisen.ipr.dto.ClientDTO;
-import ru.raiffeisen.ipr.dto.ClientUpdateDTO;
-import ru.raiffeisen.ipr.dto.ClientDeleteByEmailDTO;
+import ru.raiffeisen.ipr.dto.*;
 import ru.raiffeisen.ipr.entity.Client;
 import org.springframework.web.bind.annotation.*;
 import ru.raiffeisen.ipr.mappers.ClientMapper;
 import ru.raiffeisen.ipr.service.ClientService;
 
-import java.io.Serializable;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/clients")
@@ -21,43 +18,65 @@ public class ClientController {
     @Autowired
     private ClientService clientService;
 
-/**------SHOW ALL CLIENT OPERATION-------
- * @return**/
-    @CrossOrigin(origins = "*")
-    @RequestMapping(method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public List<Serializable> getOurClient(ClientDTO clientDTO) {
-        return Arrays.asList(
-                clientDTO.getName(),
-                clientDTO.getSurname(),
-                clientDTO.getPassword(),
-                clientDTO.getSurname(),
-                clientDTO.isFired());
-    }
+///**------SHOW ALL CLIENT OPERATION-------
+// * @return**/
+//    @CrossOrigin(origins = "*")
+//    @RequestMapping(method = RequestMethod.GET)
+//    @ResponseStatus(HttpStatus.OK)
+//    public List<Client> getOurClient() {
+//        System.out.println(clientService.getAll());
+//        return clientService.getAll();
+//    }
 
-/**------ADD NEW CLIENT OPERATION-------**/
+
+    /**
+     * ------ADD NEW CLIENT OPERATION-------
+     **/
     @CrossOrigin(origins = "*")
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    public void createClient(@RequestBody ClientDTO clientDTO) {
+    public ClientDTOAfterSave createClient(@RequestBody ClientDTO clientDTO) {
         Client client = ClientMapper.fromClientDTOToClientEntity(clientDTO);
         clientService.saveClient(client);
+        Client clientAfterSave = clientService.saveClient(client);
+        return ClientMapper.fromClientToClientDTOAfterSave(clientAfterSave);
     }
 
-/**------DELETE CLIENT BY EMAIL-------**/
+    /**
+     * ------DELETE CLIENT BY EMAIL-------
+     **/
     @CrossOrigin(origins = "*")
     @RequestMapping(method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.OK)
-    public void deleteClient(@RequestBody ClientDeleteByEmailDTO clientDeleteByEmailDTO){
+    public void deleteClient(@RequestBody ClientDeleteByEmailDTO clientDeleteByEmailDTO) {
         clientService.deleteClientByEmail(clientDeleteByEmailDTO.getEmail());
     }
 
-/**------UPDATE CLIENT BY ID------**/
+    /**
+     * ------UPDATE CLIENT BY ID------
+     **/
     @CrossOrigin(origins = "*")
     @RequestMapping(method = RequestMethod.PUT)
     @ResponseStatus(HttpStatus.CREATED)
     public void updateClient(@RequestBody ClientUpdateDTO clientUpdateDTO) {
         Client clientForSave = ClientMapper.fromClientUpdateDTOToClientEntity(clientUpdateDTO);
         clientService.saveClient(clientForSave);
+    }
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public List<ClientDTO> getClients() {
+        List<Client> all = clientService.getAll();
+        return all.stream().map(client -> new ClientDTO(
+                client.getName(),
+                client.getSurname(),
+                client.getEmail(),
+                client.isFired(),
+                client.getPassword(),
+                client.getAllIncludedPartner().stream()
+                        .map(partner -> new PartnerDTO(partner.getSupport().stream().findFirst().get().getName(), partner.getRequirements()))
+                        .collect(Collectors.toList())))
+                .collect(Collectors.toList());
     }
 }
