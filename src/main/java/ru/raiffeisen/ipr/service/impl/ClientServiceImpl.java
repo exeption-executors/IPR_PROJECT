974ea50b0d94e140662b739995f394518e4ecebd
LightWind <OLIGHTWINDO@gmail.com>
2019-06-25 10:47:10 +0300

    fix update client method

--- src/main/java/ru/raiffeisen/ipr/service/impl/ClientServiceImpl.java
+++ src/main/java/ru/raiffeisen/ipr/service/impl/ClientServiceImpl.java
@@ -64,7 +64,12 @@ public class ClientServiceImpl implements ClientService {
         if(!clientFromDB.isPresent()) {
             throw new ClientNotFoundException();
         } else {
-            return clientRepository.save(clientFromDB.get());
+            Client clientValue = clientFromDB.get();
+            clientValue.setName(client.getName());
+            clientValue.setSurname(client.getSurname());
+            clientValue.setPassword(client.getPassword());
+            clientValue.setFired(client.isFired());
+            return clientRepository.save(clientValue);
         }
     }
 
