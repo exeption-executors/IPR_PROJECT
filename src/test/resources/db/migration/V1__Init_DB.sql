4075ad2b39005a675d635288f0ab747b5c00e79c
Mark Bryzgalov <marik33409@mail.ru>
2019-06-25 17:27:05 +0300

    added PlanControllerTest

--- src/test/resources/db/migration/V1__Init_DB.sql
+++ src/test/resources/db/migration/V1__Init_DB.sql
@@ -8,14 +8,14 @@ CREATE TABLE  client (
 );
 
 CREATE TABLE  plan (
-     id BIGINT PRIMARY KEY NOT NULL,
+     id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
      client_id BIGINT NOT NULL references client(id) ON DELETE CASCADE,
      plan_date_start DATE NOT NULL,
      plan_date_end DATE NOT NULL
 );
 
 CREATE TABLE section (
-     id BIGINT PRIMARY KEY NOT NULL,
+     id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
      plan_id BIGINT NOT NULL references plan(id) ON DELETE CASCADE,
      section_date_end DATE NOT NULL,
      section_description VARCHAR(512) NOT NULL,
