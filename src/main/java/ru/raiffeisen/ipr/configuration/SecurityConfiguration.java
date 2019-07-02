6f3cab0302db82f19ef223086835fcc72b8a8775
LightWind <OLIGHTWINDO@gmail.com>
2019-07-02 17:13:28 +0300

    authorization function was added

--- src/main/java/ru/raiffeisen/ipr/configuration/SecurityConfiguration.java
+++ src/main/java/ru/raiffeisen/ipr/configuration/SecurityConfiguration.java
@@ -29,29 +29,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .usersByUsernameQuery("select email,password, true from client where email=?")
                 .authoritiesByUsernameQuery("select email, role from client where email=?");
     }
-
-
-    //    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//
-//        auth.inMemoryAuthentication()
-//                .withUser("select email from client where email=?").password("{noop}password").roles("USER")
-//                .and()
-//                .withUser("admin").password("{noop}password").roles("ADMIN");
-
-    //        @Autowired
-//        public void configAuthentication (AuthenticationManagerBuilder auth) throws Exception {
-//            auth.jdbcAuthentication()
-//                    .usersByUsernameQuery("select email,password, enabled from client where email=?");
-//        }
-//
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//            http.authorizeRequests().antMatchers("/", "/home").permitAll().antMatchers("/admin").hasRole("ADMIN")
-////                    .anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
-////                    .permitAll();
-////            http.exceptionHandling().accessDeniedPage("/403");
-        http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
     }
 
 
